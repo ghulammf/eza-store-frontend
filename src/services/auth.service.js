c52421944535547e22fe2e95ec05dbbab6fb91ba
ghulammf <muhtadinamri123@gmail.com>
2025-06-10 19:06:24 +0700

    fixing error  auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,26 +2,26 @@
 import api from '../api/api';
 
 //const API_URL = 'http://localhost:4000/api/auth'
-const API_URL = import.meta.env.VITE_API_URL;
+const URL = '/api/auth'
 
 const AuthService = {
     register: async function (username, email, password, confirm_password) {
-        const response = await api.post('/register', {
+        const response = await api.post(URL + '/register', {
             username, email, password, confirm_password
         });
         return response.data
     },
     refreshToken: async function () {
-        const response = await api.post('/refresh-token')
+        const response = await api.post(URL + '/refresh-token')
         const accessToken = response.data.data
         return accessToken
     },
     login: async function (username, password) {
-        const response = await api.post('/api/auth/login', { username, password })
+        const response = await api.post(URL + '/login', { username, password })
         return response.data
     },
     logout: async function () {
-        const response = await api.post('/logout')
+        const response = await api.post(URL + '/logout')
         return response.data
     }
 }
